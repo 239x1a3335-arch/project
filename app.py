8cc505773e1f535b5297d04790cb833a3fb3beca
BATHALA BALAJI <239x1a3335@gprec.ac.in>
2026-03-14 15:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -287,6 +287,8 @@ def generate_pdf():
 @app.route('/')
 def index():
     return render_template('index.html')
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
